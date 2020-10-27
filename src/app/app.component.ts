1aa62304ee763b63f55367010a55c32b99713ac9
Ahmad Adlouni <adlouniahmad@outlook.com>
2020-10-27 21:55:21 +0200

    Updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 import AOS from 'aos';
 
 @Component({
@@ -8,9 +9,15 @@ import AOS from 'aos';
 })
 export class AppComponent implements OnInit {
 
+  constructor(private swUpdate: SwUpdate) {}
+
   ngOnInit(): void {
     AOS.init({
       once: true
+    });
+
+    this.swUpdate.available.subscribe(e => {
+      window.location.reload()
     });
   }
 
